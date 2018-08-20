5fcbc739bab13d1210a554d6c3c8322bc3884e92
Robert Watson <spartan83@gmail.com>
2018-08-20 03:58:20 +0000

    added neccecary files including environment and all lib files

---
+++ Anichart-cli-app/config/environment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/anichart/scraper'
+require_relative '../lib/anichart/anime'
+require_relative '../lib/anichart/cli'
+require_relative '../lib/anichart/version'
